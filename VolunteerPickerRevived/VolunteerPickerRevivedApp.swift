3cebfcc9a1f77753e7196900e17621990a068963
srlbraintree <stephen.londergan@gmail.com>
2024-12-08 11:53:06 -0500

    Initial Commit

---
+++ VolunteerPickerRevived/VolunteerPickerRevivedApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolunteerPickerRevivedApp.swift
+//  VolunteerPickerRevived
+//
+//  Created by Londergan, Stephen on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct VolunteerPickerRevivedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VolunteerPickerRevivedTests/VolunteerPickerRevivedTests.swift
@@ -0,0 +1,17 @@
+//
+//  VolunteerPickerRevivedTests.swift
+//  VolunteerPickerRevivedTests
+//
+//  Created by Londergan, Stephen on 12/8/24.
+//
+
+import Testing
+@testable import VolunteerPickerRevived
+
+struct VolunteerPickerRevivedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
